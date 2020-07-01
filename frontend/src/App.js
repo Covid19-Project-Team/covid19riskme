e87fb9c296cad07597d12bc438e5a607bae7f48e
Rye J <RyeSeekins@utexas.edu>
2020-07-01 14:53:25 -0500

    Changed the home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg';gi
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
+  // highlight-next-line
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+
+
+function App() {
+  
+setInterval(tick, 1000);
+
 }
 
 export default App;
