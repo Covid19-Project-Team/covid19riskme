060fd612dad500ab77f0eb3a6880feffde7e0130
Rye J <RyeSeekins@utexas.edu>
2020-07-01 15:07:32 -0500

    Changed home again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,2 @@
-import React from 'react';
-import logo from './logo.svg';gi
-import './App.css';
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  // highlight-next-line
-  ReactDOM.render(element, document.getElementById('root'));
-}
-
-
-
-function App() {
-  
-setInterval(tick, 1000);
-
-}
-
-export default App;
+<h1>Hello, world!</h1>;
+<h2> Rye did this</h2>;
